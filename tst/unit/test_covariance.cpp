020912e5d8bf4b1756a6b03e648a9b59f2067661
JoshuaSBrown <joshua.s.brown@lanl.gov>
2021-03-21 00:14:28 -0600

    Add require statements to unit test of covariance

--- tst/unit/test_covariance.cpp
+++ tst/unit/test_covariance.cpp
@@ -21,24 +21,18 @@ TEST_CASE("Testing:covariance creation","[unit,panacea]"){
     {1.0, 4.0},
     {2.0, 5.0},
     {3.0, 6.0}};
-  //std::vector<double> val1 = {1.0, 4.0};
-  //std::vector<double> val2 = {2.0, 5.0};
-  //std::vector<double> val3 = {3.0, 6.0};
-  //data.push_back(val1);
-  //data.push_back(val2);
-  //data.push_back(val3);
 
   DescriptorWrapper<std::vector<std::vector<double>>*> dwrapper(&data, 3, 2);
 
   Covariance cov(&dwrapper);
-/*
+
   REQUIRE(cov.rows() == 2);
   REQUIRE(cov.cols() == 2);
 
   REQUIRE(cov.getMean(0) == Approx(2.0));
   REQUIRE(cov.getMean(1) == Approx(5.0));
 
-  REQUIRE(cov.getCummulativeDescPoints() == 3);*/
+  REQUIRE(cov.getCummulativeDescPoints() == 3);
 }
 
 
