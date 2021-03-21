1a294ce4902d1562abbc08cde37bf4bd74746295
JoshuaSBrown <joshua.s.brown@lanl.gov>
2021-03-21 00:12:38 -0600

    Flesh out gaussian uncorrelated test

--- tst/unit/test_gaussian_uncorrelated.cpp
+++ tst/unit/test_gaussian_uncorrelated.cpp
@@ -1,7 +1,10 @@
 
 // Local private includes
 #include "primitives/gaussian_uncorrelated.hpp"
+
+#include "attributes/covariance.hpp"
 #include "descriptors/descriptor_wrapper.hpp"
+#include "kernels/kernel_wrapper.hpp"
 
 // Third party includes
 #include <catch2/catch.hpp>
@@ -18,12 +21,21 @@ TEST_CASE("Testing:creation of gaussian uncorrelated primitive","[unit,panacea]"
 }
 
 TEST_CASE("Testing:compute of gaussian uncorrelated primitive","[unit,panacea]"){
-  std::vector<std::vector<double>> data;
 
-  std::vector<double> values = {0.0, 1.0};
-  data.push_back(values);
+  // Assumes we are dealing with two dimensions and a single point
+  std::vector<std::vector<double>> raw_desc_data;
+  std::vector<double> raw_desc_values = {0.0, 1.0};
+  raw_desc_data.push_back(raw_desc_values);
+  DescriptorWrapper<std::vector<std::vector<double>>*> dwrapper(&raw_desc_data,1, 2);
 
-  DescriptorWrapper<std::vector<std::vector<double>>*> dwrapper(&data,1, 2);
+  Covariance cov(&dwrapper);
 
+  // Assumes we are dealing with two dimensions and a single point
+  std::vector<std::vector<double>> raw_kern_data;
+  std::vector<double> raw_kern_values = {0.0, 0.5};
+  raw_kern_data.push_back(raw_kern_values);
+  KernelWrapper<std::vector<std::vector<double>>*> kwrapper(&raw_kern_data,1, 2);
 
+  PrimitiveAttributes attr;
+  attr.kernel_wrapper = &kwrapper;
 }
