7606b4ac97b58b77276781e14bd29873bdb5967b
JoshuaSBrown <joshua.s.brown@lanl.gov>
2021-04-04 23:00:54 -0600

    Add type_register back in

---
+++ src/libpanacea/type_register.hpp
@@ -0,0 +1,45 @@
+
+// Standard includes
+#include <any>
+#include <tuple>
+#include <unordered_map>
+
+namespace panacea {
+
+  template<typename ...Ts>
+  class TypeRegister {
+
+    private:
+      std::tuple<Ts...> types_;
+/*
+      template<class T, unsigned int N>
+        T returnType(const std::any val){
+          if( val.type() == typeid(std::tuple_element<N, std::tuple<Args...>>::type)){
+            return std::any_cast<std::tuple_element<N, std::tuple<Args...>>::type>(val);
+          }
+          return returnType<T,N-1>(val);
+        }
+
+      template<class T, 1>
+        T returnType(const std::any val){
+          return std::any_cast<
+            std::tuple_element<N, std::tuple<Args...>>::type 
+            >(val); 
+        }
+
+*/
+
+    public:
+      TypeRegister() {};
+
+      // This is problematic because the function signature is the same
+ /*     template<class T>
+        T convert(const std::any val){
+          return returnType<T,
+                 std::tuple_size<decltype(types_)>::value
+                 >(val);
+        }
+      */
+
+  };
+}
