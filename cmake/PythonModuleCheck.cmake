6ee828313257cc4eaf181c637147fa423017da7e
JoshuaSBrown <joshbro42867@yahoo.com>
2021-05-19 10:16:28 -0600

    Add missing cmake file

---
+++ cmake/PythonModuleCheck.cmake
@@ -0,0 +1,22 @@
+
+function(check_python_modules python_modules)
+  foreach( python_module ${python_modules})
+    if( "${python_module}" STREQUAL "pytest")
+      execute_process(COMMAND ${Python3_EXECUTABLE} -m ${python_module} --version
+        OUTPUT_VARIABLE MODULE_output
+        ERROR_VARIABLE  MODULE_error
+        RESULT_VARIABLE MODULE_result)
+      if( NOT ${MODULE_result} EQUAL 0)
+        message(SEND_ERROR "Pytest package not available: ${MODULE_error}")
+      endif()
+    else()
+      execute_process(COMMAND ${Python3_EXECUTABLE} -c "import ${python_module}"
+        OUTPUT_VARIABLE MODULE_output
+        ERROR_VARIABLE  MODULE_error
+        RESULT_VARIABLE MODULE_result)
+      if( NOT ${MODULE_result} EQUAL 0)
+        message(SEND_ERROR "${python_module} module not available: ${MODULE_error}")
+      endif()
+    endif()
+  endforeach()
+endfunction()
