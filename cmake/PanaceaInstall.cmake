24cac01d6cb91a49169146e04d55208aecf0f61f
JoshuaSBrown <joshbro42867@yahoo.com>
2021-05-26 06:48:53 -0600

    Fix installation issues

--- cmake/PanaceaInstall.cmake
+++ cmake/PanaceaInstall.cmake
@@ -1,12 +1,13 @@
 
 set_target_properties(panacea PROPERTIES VERSION ${panacea_VERSION} SOVERSION ${SOVERSION})
 
-target_include_directories(panacea PUBLIC
-  $<BUILD_INTERFACE:${panacea_SOURCE_DIR}/src/libpanacea>
-  $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/panacea>
+target_include_directories(panacea
+  PUBLIC
+    $<BUILD_INTERFACE:${panacea_SOURCE_DIR}/include/panacea>
+    $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/panacea> 
+  PRIVATE
+    $<BUILD_INTERFACE:${panacea_SOURCE_DIR}/src/libpanacea>
   )
-
-message("Install include dir ${CMAKE_INSTALL_INCLUDEDIR}/panacea")
 
 install(TARGETS panacea EXPORT panaceaTargets
   INCLUDES DESTINATION "${CMAKE_INSTALL_INCLUDEDIR}"
@@ -15,7 +16,7 @@ install(TARGETS panacea EXPORT panaceaTargets
   )
 
 # Install config files
-install(FILES ${PROJECT_SOURCE_DIR}/cmake/PanaceaConfig.cmake DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/panacea")
+install(FILES ${PROJECT_SOURCE_DIR}/cmake/panaceaConfig.cmake DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/panacea")
 
 install(EXPORT panaceaTargets
   FILE panaceaTargets.cmake
