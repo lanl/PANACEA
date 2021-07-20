fb1d4ee39206de75d8f795fa1241a1fdd6634a26
JoshuaSBrown <joshua.s.brown@lanl.gov>
2021-07-20 16:42:33 -0600

    Added numerical stability

---
+++ src/libpanacea/entropy/entropy_terms/entropy_term_common.hpp
@@ -0,0 +1,15 @@
+
+#ifndef PANACEA_PRIVATE_ENTROPYTERMCOMMON_H
+#define PANACEA_PRIVATE_ENTROPYTERMCOMMON_H
+#pragma once
+
+namespace panacea {
+
+  /**
+   * Common functions used by entropy terms.
+   **/
+  bool is_neg_inf(const double val);
+  bool is_pos_inf(const double val);
+} // namespace panacea
+
+#endif // PANACEA_PRIVATE_ENTROPYTERMCOMMON_H
